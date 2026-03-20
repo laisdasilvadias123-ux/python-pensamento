c79cfd85e7790a28399a1116f122cb3f36962e4a
laisdasilvadias123 <laisdasilvadias123@gmail.com>
2026-03-20 11:31:35 -0300

    aula 2 variaveis e operadores

---
+++ aula02a.py
@@ -0,0 +1,28 @@
+print("ola mundo")
+
+print("7 + 4")
+print("7" + "4") #concatenação de strings
+#comentario de 1 linha
+'''
+comentarios de
+multiplas linhas
+
+'''
+#variaveis
+nome = "Lais" #str
+idade = 17 #int
+peso = 10 #float
+
+print(nome, idade, peso)
+print(f"oiii {nome}!!!")
+print("oiii", nome, "!!!!")
+
+#input- simulação de um forms no cmd
+nome= input("digite o seu nome: ")
+idade= int(input("digite o seu idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
+
---
+++ testea.py
@@ -0,0 +1,5 @@
+print("Adicione seu nome!")
+
+nome= input("digite o seu nome: ")
+print(nome)
+print(f"Seja bem vindo(a) {nome}!")
