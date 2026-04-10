fca986e059eb25eae46280be346eb64f22f44949
laisdasilvadias123 <laisdasilvadias123@gmail.com>
2026-04-10 11:25:43 -0300

    aula 3-1cca

---
+++ aula 03-modulos-condicionais/aula03-modulos.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+num = 17
+raiz = sqrt(num)
+print(f"A raiz de {num} é {raiz:.2f}")
+
+import random
+
+mum_rand_int = random.randint(1,60)
+print(num_rand_int)
---
+++ aula 03-modulos-condicionais/aula03b_funcoes.py
@@ -0,0 +1,24 @@
+
+
+def print_lyrics():
+    print("I ain't gonna live forever")
+    print("I just wanna to live while I'm alive")
+
+print_lyrics()
+print() # pular uma linha
+
+def boas_vindas(nome):
+    print(f"Olá, {nome}!!! Seja bem-vindo!!")
+
+nome_digitado = input("Digite seu nome: ")
+boas_vindas(nome_digitado)
+
+def soma(num_a, num_b):
+    soma = num_a +num_b
+    return soma
+
+resultado_soma = soma(34, 78)
+print(resultado_soma)
+print(type(nome_digitado))
+
+
